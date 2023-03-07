115e4ff69b4abee7dc08a84677e13c846eae0072
cdmi123 <languagepdf@gmail.com>
2023-03-07 12:09:46 +0530

    ravi update

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var app = express();
 app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
 
-
-  app.get('/',function(req,res){
+app.get('/',function(req,res){
 
     var datestring = new Date().toISOString();
 
@@ -24,6 +23,23 @@ var year = dateObj.getUTCFullYear();
 newdate = day + " " + month + " " + year;
 
     res.render('index',{dayName,newdate});
+  })
+
+  app.get('/task',function(req,res){
+
+    var datestring = new Date().toISOString();
+
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var d = new Date(datestring);
+    var dayName = days[d.getDay()];
+
+    var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; //months from 1-12
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
+newdate = day + " " + month + " " + year;
+
+    res.render('task',{dayName,newdate});
   })
 
 
